1f3927f9282a412f541a729198a2af24d23f093e
Jazmin Acuna <jazminacunia@gmail.com>
2020-10-05 16:22:11 -0300

    Home menu sidebar v0

--- app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/activities/HomeActivity.kt
+++ app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/activities/HomeActivity.kt
@@ -1,11 +1,67 @@
 package com.utn_frba_mobile_2020_c2.safeout.activities
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.utn_frba_mobile_2020_c2.safeout.R
 
-class HomeActivity : BaseActivity() {
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_search -> {
+                Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_checkin -> {
+                Toast.makeText(this, "Checkin", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_checkout -> {
+                Toast.makeText(this, "Checkout", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_reservations -> {
+                Toast.makeText(this, "Mis Reservas", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_support -> {
+                Toast.makeText(this, "Soporte", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
