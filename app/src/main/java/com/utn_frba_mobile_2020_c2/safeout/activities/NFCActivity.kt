8da93b68a3b27b04b0c208b9d2eeb37e2d1026b3
Fsamassa <facundo.samassa@hotmail.com>
2020-10-15 19:37:50 -0300

    agrego activity y layout nfc

---
+++ app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/activities/NFCActivity.kt
@@ -0,0 +1,11 @@
+package com.utn_frba_mobile_2020_c2.safeout.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NFCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+    }
+}
