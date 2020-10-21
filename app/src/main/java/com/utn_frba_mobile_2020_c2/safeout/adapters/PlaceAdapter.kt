d93da1dc15d18ddf7ad26ab8444434858dae084c
fedebelve <federicobelvedere@ymail.com>
2020-10-21 00:26:13 -0300

    agrego busqueda en el menu de arriba. Falta consulta a la API

--- app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/adapters/PlaceAdapter.kt
+++ app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/adapters/PlaceAdapter.kt
@@ -1,20 +1,38 @@
 package com.utn_frba_mobile_2020_c2.safeout.adapters
 
+import android.location.Location
+import android.location.LocationManager
+import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import com.utn_frba_mobile_2020_c2.safeout.R
 import com.utn_frba_mobile_2020_c2.safeout.listeners.RecyclerPlaceListener
 import com.utn_frba_mobile_2020_c2.safeout.models.Place
 import com.utn_frba_mobile_2020_c2.safeout.extensions.inflate
+import com.utn_frba_mobile_2020_c2.safeout.extensions.toast
+import com.utn_frba_mobile_2020_c2.safeout.models.Section
+import kotlinx.android.synthetic.main.fragment_placelist.view.*
 import kotlinx.android.synthetic.main.recycler_place.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
-class PlaceAdapter(private val places:List<Place>, private val listener: RecyclerPlaceListener)
-    : RecyclerView.Adapter<PlaceAdapter.ViewHolder>(){
+class PlaceAdapter(private var places:List<Place>, private val listener: RecyclerPlaceListener)
+    : RecyclerView.Adapter<PlaceAdapter.ViewHolder>(), Filterable{
+
+    private var placesFilterList: ArrayList<Place> = places as ArrayList<Place>
+
 
     //donde va el reemplazo de los textView con el texto que llega del objeto. Se llama por elemento
     // el View es al CardView entero.
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+
         fun bind(place: Place, listener: RecyclerPlaceListener) = with(itemView){
             textViewName.text = place.name
             textViewAddress.text = place.address
@@ -25,7 +43,9 @@ class PlaceAdapter(private val places:List<Place>, private val listener: Recycle
             setOnClickListener { listener.onClick(place, adapterPosition) }
 
         }
+
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(parent.inflate(
         R.layout.recycler_place))
@@ -33,4 +53,66 @@ class PlaceAdapter(private val places:List<Place>, private val listener: Recycle
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(places[position], listener)
 
     override fun getItemCount() = places.size
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                val queryString = charSequence?.toString()?.toLowerCase()
+
+                val filterResults = FilterResults()
+                filterResults.values = if (queryString==null || queryString.isEmpty())
+                    placesFilterList
+                else
+
+                    placesFilterList.filter {
+                        it.name.toLowerCase().contains(queryString.toLowerCase()) ||
+                                it.address.toLowerCase().contains(queryString.toLowerCase()) ||
+                                it.category.toLowerCase().contains(queryString.toLowerCase())
+                    }
+                return filterResults
+
+        }
+            /*override fun performFiltering(constraint: CharSequence?): FilterResults? {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    print("LLLLLL")
+                    placesFilterList = places as ArrayList<Place>
+                } else {
+                    println("PPPPP")
+                    val resultList = ArrayList<Place>()
+                    //placesFilterList.clear()
+                    for (row in places) {
+                        if (row.name.toLowerCase(Locale.ROOT).contains(charSearch.toLowerCase(Locale.ROOT))) {
+                            println("UUUU")
+                            resultList.add(row)
+                        }
+                    }
+                    print(resultList)
+                    placesFilterList = resultList
+                }
+                val filterResults = FilterResults()
+                println("MMMM")
+                println(placesFilterList)
+                println("CCCC")
+                filterResults.values = placesFilterList
+                print(filterResults)
+
+                return filterResults
+            }*/
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+
+                //placesFilterList.clear()
+                //placesFilterList = results.values as ArrayList<Place>
+
+                places = results!!.values as ArrayList<Place>
+                notifyDataSetChanged()
+
+            }
+    }
+
+}
+
+
 }
--- app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/fragments/PlaceListFragment.kt
+++ app/src/main/java/com/utn_frba_mobile_2020_c2/safeout/fragments/PlaceListFragment.kt
@@ -3,20 +3,23 @@ package com.utn_frba_mobile_2020_c2.safeout.fragments
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.*
+import android.app.SearchManager
+import androidx.appcompat.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.utn_frba_mobile_2020_c2.safeout.R
+import com.utn_frba_mobile_2020_c2.safeout.adapters.PlaceAdapter
+import com.utn_frba_mobile_2020_c2.safeout.extensions.toast
 import com.utn_frba_mobile_2020_c2.safeout.listeners.RecyclerPlaceListener
 import com.utn_frba_mobile_2020_c2.safeout.models.Place
-import com.utn_frba_mobile_2020_c2.safeout.extensions.toast
-import com.utn_frba_mobile_2020_c2.safeout.adapters.PlaceAdapter
 import com.utn_frba_mobile_2020_c2.safeout.models.Section
+import kotlinx.android.synthetic.main.fragment_placelist.*
 import kotlinx.android.synthetic.main.fragment_placelist.view.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -39,21 +42,73 @@ class PlaceListFragment : Fragment() {
                 val seccion = Section("Patio", 20)
                 var secciones: MutableList<Section> = arrayListOf(seccion)
 
-                add(Place(1, "Siga la Vaca","Perro 123", "Bar", R.drawable.resto, targetlocation, secciones))
-                add(Place(2, "El Club de la Milanesa","Gato 123", "Universidad", R.drawable.resto, targetlocation, secciones))
-                add(Place(3, "Cesto","Pikachu 123", "Biblioteca", R.drawable.resto, targetlocation, secciones))
-                add(Place(4, "Guerrin","Ratata 123", "Bar", R.drawable.resto, targetlocation, secciones))
-                add(Place(5, "El Antojo","Raichu 123", "Supermercado", R.drawable.resto, targetlocation, secciones))
+                add(
+                    Place(
+                        1,
+                        "Siga la Vaca",
+                        "Perro 123",
+                        "Bar",
+                        R.drawable.resto,
+                        targetlocation,
+                        secciones
+                    )
+                )
+                add(
+                    Place(
+                        2,
+                        "El Club de la Milanesa",
+                        "Gato 123",
+                        "Universidad",
+                        R.drawable.resto,
+                        targetlocation,
+                        secciones
+                    )
+                )
+                add(
+                    Place(
+                        3,
+                        "Cesto",
+                        "Pikachu 123",
+                        "Biblioteca",
+                        R.drawable.resto,
+                        targetlocation,
+                        secciones
+                    )
+                )
+                add(
+                    Place(
+                        4,
+                        "Guerrin",
+                        "Ratata 123",
+                        "Bar",
+                        R.drawable.resto,
+                        targetlocation,
+                        secciones
+                    )
+                )
+                add(
+                    Place(
+                        5,
+                        "El Antojo",
+                        "Raichu 123",
+                        "Supermercado",
+                        R.drawable.resto,
+                        targetlocation,
+                        secciones
+                    )
+                )
             }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         arguments?.let {
             //param1 = it.getString(ARG_PARAM1)
             //param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -65,6 +120,7 @@ class PlaceListFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_placelist, container, false)
         recycler = rootView.recyclerView as RecyclerView
         setRecyclerView()
+
         return rootView
     }
 
@@ -80,6 +136,36 @@ class PlaceListFragment : Fragment() {
             }
 
         }))
+
         recycler.adapter = adapter
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        print("HHHHH")
+        inflater.inflate(R.menu.main_menu, menu)
+
+        val menuItem = menu!!.findItem(R.id.search)
+
+        val searchView = menuItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //list.clear()
+                adapter.filter.filter(newText)
+                adapter.notifyDataSetChanged()
+                return true
+            }
+
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
     }
